5de013a872c4b3e61d5a6c067895f08fbe482712
leksandre <44774042+leksandre@users.noreply.github.com>
2023-06-06 13:27:40 +0500

    Add files via upload

--- crabWeb2.js
+++ crabWeb2.js
@@ -1,60 +1,72 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-(async function() {
-for (let i2 = 0; i2 < 999; i2++) {
+function choose(choices) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
 var aTags;
-var searchText;
 var found;
-searchText = "ещё комментари";
-aTags = document.getElementsByTagName("div");
-aBtns = document.getElementsByClassName("button_width_100 comments__more-button comments_fold");
-for (var i = 0; i < aBtns.length; i++) {
-  found = aBtns[i];
-  await found.click();
-  await sleep( 1000);
-}
-if((Math.random() < 0.5)){
-  aTags = document.getElementsByClassName("comment-toggle-children__label");
-}else{
-  aTags = document.getElementsByClassName("comment-hidden-group__toggle");
-}
-if((Math.random() < 0.5)){
-aTags = document.getElementsByClassName("comment-hidden-group");
-}
-if((Math.random() < 0.5)){
-aTags = document.getElementsByClassName("comment-toggle-children_collapse");
-}
+(async function () {
+  for (let i2 = 0; i2 < 9999; i2++) {
+    let choose1 = choose([
+      "comment-hidden-group__toggle",
+      // "comment__more",
+      // "comment-toggle-children__icon",
+      // "comment-toggle-children ",
+      // "comment-hidden-group",
+      // "comments__more-count",
+      // "comment-toggle-children_collapse",
+      // "comment-hidden-group",
+      "comment-toggle-children__label",
+      "comments__more-button"
+    ])
+    aTags = document.getElementsByClassName(choose1);
+    var wasopened = false;
+    for (var i = 0; i < aTags.length; i++) {
+      var havetoClick = false;
 
-for (var i = 0; i < aTags.length; i++) {
-var havetoClick = false;
-for (var child1 of aTags[i].parentElement.children) {
-if (child1.visibility === "hidden" || child1.hidden) {
-  havetoClick = true;
-}
-}
-for (var child1 of aTags[i].parentElement.parentElement.children) {
-if (child1.visibility === "hidden" || child1.hidden) {
-  havetoClick = true;
-}
-}
-for (var child1 of aTags[i].parentElement.parentElement.parentElement.children) {
-if (child1.visibility === "hidden" || child1.hidden) {
-  havetoClick = true;
-}
-}
-if(aTags[i].classList.contains("mystyle11")){
-  havetoClick = false;
-}
-if (havetoClick) {
-  found = aTags[i];
-  await found.click();
-  found.classList.add("mystyle11");
-  await sleep( 100);
-}
 
-}
-await sleep( 1000);
-}
+
+      if (aTags[i].classList.contains("comment-hidden-group__toggle")) {
+        havetoClick = true;
+      }
+
+      if (aTags[i].classList.contains("mystyle11")) {
+        havetoClick = false;
+      }
+
+
+      for (var child1 of aTags[i].parentElement.children) {
+        if (child1.classList.contains("comment__children") || child1.classList.contains("comment-hidden-group__children"))
+          if (child1.visibility === "hidden" || child1.hidden) {
+            havetoClick = true;
+          }
+      }
+      for (var child1 of aTags[i].parentElement.parentElement.children) {
+        if (child1.classList.contains("comment__children") || child1.classList.contains("comment-hidden-group__children"))
+          if (child1.visibility === "hidden" || child1.hidden) {
+            havetoClick = true;
+          }
+      }
+      for (var child1 of aTags[i].parentElement.parentElement.parentElement.children) {
+        if (child1.classList.contains("comment__children") || child1.classList.contains("comment-hidden-group__children"))
+          if (child1.visibility === "hidden" || child1.hidden) {
+            havetoClick = true;
+          }
+      }
+
+
+      if (havetoClick) {
+        found = aTags[i];
+        await found.click();
+        found.classList.add("mystyle11");
+        wasopened = true;
+        await sleep(100);
+
+      }
+    }
+    if (wasopened)
+      await sleep(3000);
+  }
 })();
